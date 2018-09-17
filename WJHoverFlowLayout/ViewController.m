af70cb084f7a28ed0844fe76a4bbb1351f9d1598
wangjiegit <wangjie@wangjiedeMacBook-Pro.local>
2018-09-17 22:10:36 +0800

    gif提交

--- WJHoverFlowLayout/ViewController.m
+++ WJHoverFlowLayout/ViewController.m
@@ -39,10 +39,15 @@
 #pragma mark UICollectionViewDataSource
 
 - (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
-    return 10;
+    return 3;
 }
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    if (section == 0) {
+        return 6;
+    } else if (section == 1) {
+        return 12;
+    }
     return 10;
 }
 
@@ -54,6 +59,14 @@
 
 - (UICollectionReusableView *)collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath {
     UICollectionReusableView *reusableView = [collectionView dequeueReusableSupplementaryViewOfKind:kind withReuseIdentifier:NSStringFromClass([UICollectionReusableView class]) forIndexPath:indexPath];
+    UILabel *label = [reusableView viewWithTag:10000];
+    if (!label) {
+        label = [[UILabel alloc] init];
+        label.text = @"我是sectionHeaderView,第二个悬浮";
+        label.frame = CGRectMake(10, 0, 300, 45);
+        label.tag = 10000;
+        [reusableView addSubview:label];
+    }
     reusableView.backgroundColor = [UIColor redColor];
     return reusableView;
 }
